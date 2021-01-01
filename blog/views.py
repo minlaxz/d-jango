fd94b9dd37bc3b5dd58cabc741ead36424a39e8e
minlaxz <minminlaxz@gmail.com>
2021-01-01 13:54:42 +0630

    Rendering objects in template by each.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Blog as Post
+from pylaxz.utils import logxs
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    logxs.printf('Got request :', _int=True)
+    logxs.printf(request)
+
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'postsss':posts})
+
